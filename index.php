40b09ce420fb415e59d0cc07b3e4328b4545da23
Tina Ostrander <tostrander@greenriver.edu>
2017-05-16 22:00:41 -0500

    Added templating variables

--- index.php
+++ index.php
@@ -10,8 +10,23 @@
     //Default route
     $f3->route('GET /', function($f3) {
         
-        echo "Hello!";
+        $f3->set('username', 'jshmo');
+        $f3->set('password', sha1('Password01'));
+        $f3->set('title', 'Working with Templates');
+        $f3->set('temp', 68);
+        $f3->set('temp2', 15);
         
+        //arrays
+        $f3->set('bookmarks', array( 'http://www.cnet.com',
+                                                    'http://www.reddit.com/r/news',
+                                                    'http://edition.cnn.com/sport'));
+        
+         $f3->set('desserts', array("chocolate" => "Chocolate Mousse",
+                                    "vanilla" => "Vanilla Custard",
+                                    "strawberry" => "Strawberry Shortcake"
+                                    ));
+        
+        echo Template::instance()->render('pages/info.html');
     });
 
     //Run fat free
--- tmp/c9vb2hv1x190.2hbmt1l6jeqsc.php
+++ tmp/c9vb2hv1x190.2hbmt1l6jeqsc.php
@@ -3,11 +3,28 @@
 <html>
     <head>
         <meta charset="utf-8">
-
+        <title><?= $title ?></title>
     </head>
     <body>
-      
+        <h1><?= $title ?></h1>
+        <p>User logged in as <?= $username ?> using <?= $password ?></p>
  
+        <!-- Expressions -->
+        <h3>Temperature</h3>
+        <p>Fahrenheit: <?= $temp ?> degrees</p>
+        <p>Celsius: <?= $temp - 32 * (5.0/9.0) ?> degrees</p>
+        <p>Temp Total: <?= $temp + $temp2 ?></p>
         
+        <!-- Looping over arrays -->
+        <h3>Bookmarks</h3>
+        <p>First bookmark: <a href="<?= $bookmarks[0] ?>"><?= $bookmarks[0] ?></a></p>
+        <?php foreach (($bookmarks?:[]) as $bookmark): ?>
+            <li><a href="<?= $bookmark ?>"> <?= str_replace('http://', '', $bookmark); ?></a></li>
+        <?php endforeach; ?>
+        
+        <h3>Dessert Options</h3>
+        <?php foreach (($desserts?:[]) as $key=>$value): ?>
+            <input type='checkbox' value='<?= $key ?>'/><?= $value ?></br>
+        <?php endforeach; ?>
     </body>
 </html>
